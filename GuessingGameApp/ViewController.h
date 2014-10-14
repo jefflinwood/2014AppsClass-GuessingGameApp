264b0015810a03a7f8a387055ff32684badf3ce1
Jeffrey Linwood <jlinwood@gmail.com>
2014-10-14 14:05:14 -0500

    Initial Commit

---
+++ GuessingGameApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GuessingGameApp
+//
+//  Created by Jeffrey Linwood on 10/14/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
