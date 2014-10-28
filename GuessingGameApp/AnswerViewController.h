22cea940819ceafd54b7accecff8bf5e4e33a7fd
Jeffrey Linwood <jlinwood@gmail.com>
2014-10-28 10:57:30 -0500

    Custom View Controller classes

---
+++ GuessingGameApp/AnswerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnswerViewController.h
+//  GuessingGameApp
+//
+//  Created by Jeffrey Linwood on 10/14/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerViewController : UIViewController
+
+@end
